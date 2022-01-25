cddd83d6d72b2469ffc69652635ec6fc02722e4e
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2022-01-25 14:01:23 -0600

    deploy

---
+++ build/precache-manifest.cf639b59d3f0aedff26810ac1da95815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2d05b072295a2717dc2de772cdf46b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc26f88f21fa71678516",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "808c97a4c739a2a7a697",
+    "url": "./static/css/main.cb07c5d8.chunk.css"
+  },
+  {
+    "revision": "fc26f88f21fa71678516",
+    "url": "./static/js/2.82b8d9d1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.82b8d9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808c97a4c739a2a7a697",
+    "url": "./static/js/main.b5f7f928.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
